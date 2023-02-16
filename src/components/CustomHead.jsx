7fe362cc5f90614d4c33deba14aa33f0d4c15103
Sebastian Morgado <sebaah89@gmail.com>
2023-02-16 03:42:59 -0300

    Fix: clase agregada en Head causaba error. fue cambiada a _document.js

--- src/components/CustomHead.jsx
+++ src/components/CustomHead.jsx
@@ -7,7 +7,6 @@ export function CustomHead() {
       <meta name="description" content="A small crypto app to track bitcoin price." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/btc.png" />
-      <html data-bs-theme="dark" />
     </Head>
   )
 }
